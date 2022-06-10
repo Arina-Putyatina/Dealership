eeb55e590f5567603648de80c2d7a55694ff6309
Arina-Putyatina <popowa.arina@yandex.ru>
2022-06-10 17:22:12 +0300

    Вторая задача

--- src/main/java/Seller.java
+++ src/main/java/Seller.java
@@ -1,36 +1,49 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Seller {
 
     private final Dealership dealership;
+    private final Lock lock;
+    private final Condition condition;
 
     public Seller(Dealership dealership) {
+
         this.dealership = dealership;
+        lock = new ReentrantLock();
+        condition = lock.newCondition();
     }
 
-    public synchronized void receiveCar() {
+    public void receiveCar() {
         try {
+            lock.lock();
             System.out.printf("%s выпустил 1 авто \n", Thread.currentThread().getName());
             int timeAcceptance = 200;
             Thread.sleep(timeAcceptance);
             dealership.getCars().add(new Car());
-            notifyAll();
-        } catch (InterruptedException ex) {
+            condition.signal();
+        } catch (InterruptedException | IllegalMonitorStateException ex) {
             ex.printStackTrace();
+        } finally {
+            lock.unlock();
         }
 
     }
 
-    public synchronized Car sellCar() {
+    public Car sellCar() {
 
         try {
             if (Thread.currentThread().isInterrupted()) {
                 throw new InterruptedException();
             }
             try {
+                lock.lock();
                 System.out.printf("%s зашел в салон\n", Thread.currentThread().getName());
                 while (dealership.getCars().size() == 0 && !Thread.currentThread().isInterrupted()) {
                     System.out.println("Машин нет");
                     int waitTime = 400;
-                    wait(waitTime);
+                    condition.wait(waitTime);
                 }
                 int timeSale = 300;
                 Thread.sleep(timeSale);
@@ -38,9 +51,12 @@ public class Seller {
                 dealership.addSold();
                 return dealership.getCars().remove(0);
 
-            } catch (InterruptedException ex) {
+            } catch (InterruptedException | IllegalMonitorStateException ex) {
                 return null;
+            } finally {
+                lock.unlock();
             }
+
         } catch (InterruptedException e) {
             return null;
         }
