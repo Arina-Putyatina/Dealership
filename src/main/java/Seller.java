dc54b97e7c62d91a322bcd0de94631d45555e6df
Arina-Putyatina <popowa.arina@yandex.ru>
2022-06-18 16:53:07 +0300

    Исправления

--- src/main/java/Seller.java
+++ src/main/java/Seller.java
@@ -1,46 +1,49 @@
 public class Seller {
 
+    static final int WAIT_TIME = 1000;
     private final Dealership dealership;
 
     public Seller(Dealership dealership) {
         this.dealership = dealership;
     }
 
-    public synchronized void receiveCar() {
-        try {
-            System.out.printf("%s выпустил 1 авто \n", Thread.currentThread().getName());
-            int timeAcceptance = 200;
-            Thread.sleep(timeAcceptance);
-            dealership.getCars().add(new Car());
-            notifyAll();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
+    public void receiveCar() {
+        int needSell = dealership.getNeedSell();
+        while (dealership.getSold() < needSell) {
+            try {
+                synchronized (this) {
+                    System.out.printf("%s выпустил 1 авто \n", Thread.currentThread().getName());
+                    notify();
+                }
+                Thread.sleep(WAIT_TIME);
+                dealership.getCars().add(new Car());
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
-
     }
 
     public synchronized Car sellCar() {
 
         try {
             if (Thread.currentThread().isInterrupted()) {
-                throw new InterruptedException();
+                return null;
             }
-            try {
-                System.out.printf("%s зашел в салон\n", Thread.currentThread().getName());
-                while (dealership.getCars().size() == 0 && !Thread.currentThread().isInterrupted()) {
-                    System.out.println("Машин нет");
-                    int waitTime = 400;
-                    wait(waitTime);
-                }
-                int timeSale = 300;
-                Thread.sleep(timeSale);
+            int needSell = dealership.getNeedSell();
+            System.out.printf("%s зашел в салон\n", Thread.currentThread().getName());
+            while (dealership.getCars().size() == 0 && !Thread.currentThread().isInterrupted() && dealership.getSold() < needSell) {
+                System.out.println("Машин нет");
+                wait(WAIT_TIME);
+            }
+            if (dealership.getCars().size() > 0) {
                 System.out.printf("%s купил машину\n", Thread.currentThread().getName());
                 dealership.addSold();
                 return dealership.getCars().remove(0);
-
-            } catch (InterruptedException ex) {
+            } else {
+                System.out.println("Машин нет");
                 return null;
             }
+
         } catch (InterruptedException e) {
             return null;
         }
